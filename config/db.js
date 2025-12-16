0d0be60f12829c4d9fb3704bc601edd42b0a548b
PBS-Alekhya <152136977+PBS-Alekhya@users.noreply.github.com>
2025-12-16 12:26:37 +0530

    Fix package.json dependencies

--- config/db.js
+++ config/db.js
@@ -2,15 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    // Check if we already have a connection (Prevents crashing on Vercel)
+    
     if (mongoose.connection.readyState >= 1) {
       return;
     }
 
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
