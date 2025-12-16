1bb87723ba8647db2eb81c776ec3487bd7c592b5
PBS-Alekhya <152136977+PBS-Alekhya@users.noreply.github.com>
2025-12-16 14:59:30 +0530

    Fix db crash and server structure

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const cors = require('cors')
-const connectDB = require('./config/db')
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const eventRoutes = require('./routes/eventRoutes');
 const registrationRoutes = require('./routes/registrationRoutes');
@@ -11,32 +10,22 @@ dotenv.config();
 // Connect to Database
 connectDB();
 
-const app = express()
+const app = express();
 
-// Middleware
+
 app.use((req, res, next) => {
-  
   res.setHeader("Access-Control-Allow-Origin", "*");
-  
-  
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  
-  // Allow all standard methods
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
-
   next();
 });
 
-
-app.options("*", (req, res) => {
-  res.sendStatus(200);
-});
 app.use(express.json());
 
 // Routes
@@ -45,8 +34,14 @@ app.use('/api/events', eventRoutes);
 app.use('/api/registrations', registrationRoutes);
 
 // Test Route
-app.get('/',(req,res)=> {
+app.get('/', (req, res) => {
     res.send("API is running...");
+});
+
+
+app.options('*', (req, res) => {
+  
+  res.sendStatus(200); 
 });
 
 const PORT = process.env.PORT || 5000;
