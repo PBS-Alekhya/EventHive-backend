2d02baabc4b7de409236641c532584b3fd090e2d
PBS-Alekhya <152136977+PBS-Alekhya@users.noreply.github.com>
2025-12-16 19:08:34 +0530

    Fix cors issue12

--- server.js
+++ server.js
@@ -4,28 +4,39 @@ const connectDB = require('./config/db');
 
 const app = express();
 
+/* -------------------- CORS -------------------- */
+
+const allowedOrigins = [
+  'https://event-hive-front-end.vercel.app',
+  'https://event-hive-front-her1t91rb-pbs-alekhyas-projects.vercel.app',
+];
+
 app.use(cors({
-  origin: 'https://event-hive-front-end.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
+// Enable preflight for all routes
+app.options('*', cors());
+
+/* -------------------- BODY PARSER -------------------- */
+
 app.use(express.json());
 
-app.options('/api/auth/signup', cors());
+/* -------------------- DB CONNECTION -------------------- */
 
-//  Connect DB 
-app.use(async (req, res, next) => {
-  try {
-    await connectDB();
-    next();
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'DB connection failed' });
-  }
-});
+// Connect once (serverless-safe)
+connectDB();
 
-// Routes
+/* -------------------- ROUTES -------------------- */
+
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/events', require('./routes/eventRoutes'));
 app.use('/api/registrations', require('./routes/registrationRoutes'));
@@ -33,6 +44,5 @@ app.use('/api/registrations', require('./routes/registrationRoutes'));
 app.get('/', (req, res) => {
   res.status(200).send('API running');
 });
-
 
 module.exports = app;
