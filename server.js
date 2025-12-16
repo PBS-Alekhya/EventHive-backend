17883f519fa92949667c2e3439ef666c5abaee15
PBS-Alekhya <152136977+PBS-Alekhya@users.noreply.github.com>
2025-12-16 15:42:25 +0530

    Fix cors issue

--- server.js
+++ server.js
@@ -1,53 +1,40 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const eventRoutes = require('./routes/eventRoutes');
-const registrationRoutes = require('./routes/registrationRoutes');
 
 dotenv.config();
-
-// Connect to Database
 connectDB();
 
 const app = express();
 
+//  CORS FIRST
+app.use(cors({
+  origin: [
+    'http://localhost:5000',
+    'http://localhost:5173',
+    'https://event-hive-backend-litqjn8lk-pbs-alekhyas-projects.vercel.app'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
-  next();
-});
-
+//  Body parser AFTER CORS
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/events', eventRoutes);
-app.use('/api/registrations', registrationRoutes);
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/events', require('./routes/eventRoutes'));
+app.use('/api/registrations', require('./routes/registrationRoutes'));
 
-// Test Route
 app.get('/', (req, res) => {
-    res.send("API is running...");
-});
-
-
-app.options('*', (req, res) => {
-  
-  res.sendStatus(200); 
+  res.send('API is running...');
 });
 
 const PORT = process.env.PORT || 5000;
 
 if (require.main === module) {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
 
 module.exports = app;
