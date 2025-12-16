61bf29711903d6a1b68c95c2ed0d71d81b28146b
PBS-Alekhya <152136977+PBS-Alekhya@users.noreply.github.com>
2025-12-16 12:01:57 +0530

    Fix server.js for Vercel deployment

--- server.js
+++ server.js
@@ -8,21 +8,32 @@ const registrationRoutes = require('./routes/registrationRoutes');
 
 dotenv.config();
 
+// Connect to Database
 connectDB();
 
 const app = express()
 
+// Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PATCH", "DELETE", "PUT"]
+}));
+
+// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
 app.use('/api/registrations', registrationRoutes);
 
+// Test Route
 app.get('/',(req,res)=> {
     res.send("API is running...");
 });
 
 const PORT = process.env.PORT || 5000;
 
-module.exports = app;
+if (require.main === module) {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 
+module.exports = app;
